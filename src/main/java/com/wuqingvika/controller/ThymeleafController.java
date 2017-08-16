6d7ca3d0bcb65a3c1aaa0c395d0e10733455b006
wuqingvika <526288022@qq.com>
2017-08-16 21:45:17 +0800

    thymeleaf模板

---
+++ src/main/java/com/wuqingvika/controller/ThymeleafController.java
@@ -0,0 +1,20 @@
+package com.wuqingvika.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by wuqingvika on 2017/8/16.
+ */
+@Controller
+public class ThymeleafController {
+    @RequestMapping(value = "/thymeleaf",method = RequestMethod.GET)
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://weibo.com/wuqingvika/");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+}
--- src/test/java/com/wuqingvika/controller/GirlControllerTest.java
+++ src/test/java/com/wuqingvika/controller/GirlControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.function.Consumer;
+
 import static org.junit.Assert.*;
 
 /**
@@ -30,4 +32,24 @@ public class GirlControllerTest {
         ;//设置期望值
     }
 
+    @Test
+    public void TestLambda(){
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello wuqing!");
+            }
+        };
+        runnable.run();
+        System.out.println("----------不听不听 王八念经-----------");
+        Runnable runnable1=()->System.out.print("hello lambda");
+        runnable1.run();
+    }
+
+    @Test
+    public void TestLambda2(){
+        //Consumer<String> consumer=(x)->System.out.print(x);
+        Consumer<String> consumer=x->System.out.print(x);//如果只有一个参数小括号可以不写。
+        consumer.accept("吴庆在学Lambda");
+    }
 }
