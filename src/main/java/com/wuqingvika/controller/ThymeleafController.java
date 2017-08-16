d1034e613aeb99e84f72d0ff0a995bbd9117104c
wuqingvika <526288022@qq.com>
2017-08-16 22:52:57 +0800

    velocity (springboot1.5.x不支持)

--- src/main/java/com/wuqingvika/controller/ThymeleafController.java
+++ src/main/java/com/wuqingvika/controller/ThymeleafController.java
@@ -17,7 +17,6 @@ public class ThymeleafController {
         // return模板文件的名称，对应src/main/resources/templates/index.html
         return "index";
     }
-
     @RequestMapping(value = "/freemarker",method = RequestMethod.GET)
     public String freemarker(ModelMap map) {
         // 加入一个属性，用来在模板中读取
@@ -25,4 +24,11 @@ public class ThymeleafController {
         // return模板文件的名称，对应src/main/resources/templates/index.html
         return "freemarker";
     }
+   /* @RequestMapping(value = "/velocity",method = RequestMethod.GET)
+    public String velocity(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://weibo.com/wuqingvika/velocity");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "velocity";
+    }*/
 }
